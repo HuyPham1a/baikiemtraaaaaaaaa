4b0c7cce8384ca583e59422da98967ec6811d14a
HuyPham1a <144685387+HuyPham1a@users.noreply.github.com>
2024-03-01 20:21:08 +0700

    Create add.contact.php

---
+++ add.contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thêm Liên Hệ</title>
+</head>
+<body>
+    <h2>Thêm Liên Hệ</h2>
+    <form action="add_contact_action.php" method="post">
+        Tên: <input type="text" name="name" required><br><br>
+        Số Điện Thoại: <input type="text" name="phone_number" required><br><br>
+        <input type="submit" name="submit" value="Thêm">
+    </form>
+</body>
+</html>
